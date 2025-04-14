b176f00eb0c2d80d7d9b069f4e235bd80ef66f08
이건호 <dlrjsgh367@gmail.com>
2025-04-14 12:04:46 +0900

    Feat: 헤더 추가

---
+++ src/views/PageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="container-common">
+    <div class="pv12 ph24">
+        <div class="box-flex">
+            <div class="ml-auto">dasd</div>
+            <div>dasd</div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "PageHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
