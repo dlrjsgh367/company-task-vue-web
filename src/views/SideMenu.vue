c6dc3b53bf0ac3d16cd6374682b8d1ccf41065f9
이건호 <dlrjsgh367@gmail.com>
2025-04-15 10:21:35 +0900

    Fix: 사이드메뉴 클릭으로 페이지 이동

--- src/views/SideMenu.vue
+++ src/views/SideMenu.vue
@@ -4,20 +4,20 @@
             <div class="fw700 fs22 c5D586C">관리자페이지</div>
         </div>
         <div class="box-tab-list">
-            <div class="box-tab active">
-                <div class="icon" :class="true ? 'icon-calender-white' : 'icon-calender-gray'"></div>
+            <div class="box-tab" :class="{ 'active': $route.path === '/' }" @click="$router.push('/')">
+                <div class="icon" :class="$route.path === '/' ? 'icon-calender-white' : 'icon-calender-gray'"></div>
                 <div class="text">Dashboard</div>
             </div>
-            <div class="box-tab">
-                <div class="icon" :class="false ? 'icon-people-white' : 'icon-people-gray'"></div>
+            <div class="box-tab" :class="{ 'active': $route.path === '/user' }" @click="$router.push('/user')">
+                <div class="icon" :class="$route.path === '/user' ? 'icon-people-white' : 'icon-people-gray'"></div>
                 <div class="text">고객관리</div>
             </div>
-            <div class="box-tab">
-                <div class="icon" :class="false ? 'icon-setting-white' : 'icon-setting-gray'"></div>
+            <div class="box-tab" :class="{ 'active': $route.path === '/setting' }" @click="$router.push('/setting')">
+                <div class="icon" :class="$route.path === '/setting' ? 'icon-setting-white' : 'icon-setting-gray'"></div>
                 <div class="text">공연관리</div>
             </div>
-            <div class="box-tab">
-                <div class="icon" :class="false ? 'icon-file-white' : 'icon-file-gray'"></div>
+            <div class="box-tab" :class="{ 'active': $route.path === '/buy' }" @click="$router.push('/buy')">
+                <div class="icon" :class="$route.path === '/buy' ? 'icon-file-white' : 'icon-file-gray'"></div>
                 <div class="text">모니터링/현장구매</div>
             </div>
         </div>
